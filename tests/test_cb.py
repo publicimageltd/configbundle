0851bf3e3beb116a6903693c929669732d0535fb
Jörg Volbers <joerg@joergvolbers.de>
2023-12-10 21:45:50 +0100

    Add skeleton tests

--- tests/test_cb.py
+++ tests/test_cb.py
@@ -15,6 +15,8 @@ IGNORE_BUNDLE_ARG = 'ignore_this_arg'
 # -----------------------------------------------------------
 # FIXTURES
 
+# Function-local fixtures:
+#
 @pytest.fixture
 def test_text_file(tmp_path: Path) -> Path:
     filename = tmp_path / "textfiles"
@@ -34,44 +36,32 @@ def empty_dir(tmp_path: Path) -> Path:
     return filename
 
 
-@pytest.fixture(scope="session")
-def test_text_file_names() -> list[str]:
-    return ['one.config', 'two.rc', '.three']
+@pytest.fixture
+def empty_repo(empty_dir: Path) -> Path:
+    """Return an empty repo and monkeypatch get_repo."""
+    monkeypatch.setattr(cb, "get_repo", lambda: empty_dir)
+    return empty_dir
 
 
-@pytest.fixture(scope="session")
-def test_bin_file_names() -> list[str]:
-    return ['binary', 'startme']
+@pytest.fixture
+def empty_bundle(empty_repo: Path) -> Path:
+    """Return an empty bundle and monkeypatch get_bundle."""
+    bundle = empty_repo / "testbundle"
+    monkeypatch.setattr(cb, "get_bundle", lambda: bundle)
+    return bundle
 
-
-@pytest.fixture(scope="session")
-def test_files(test_text_file_names,
-               test_bin_file_names,
-               tmp_path_factory: pytest.TempPathFactory) -> list[Path]:
-    """Create a directory with pseudo text and binary files."""
-    filedir: Path = tmp_path_factory.mktemp("files")
-    res = []
-    for name in test_text_file_names:
-        filename = filedir / name
-        with open(filename, 'w') as file:
-            file.writelines('dummy content')
-            res.append(filename)
-
-    for name in test_bin_file_names:
-        filename = filedir / name
-        with open(filename, 'w') as file:
-            file.writelines(["#!/usr/bin/bash", "echo 'hallo'"])
-        filename.chmod(0o777)
-    return res
+# Session-wide fixtures:
+#
+# NONE
 
 
 # -----------------------------------------------------------
 # TESTS
-
+#
+# Tests for internal functions:
 
 def test_get_bundles(empty_dir, monkeypatch):
-    """Test _get_bundles: if bundle dirs are found."""
-    monkeypatch.setattr(cb, "get_repo", lambda: empty_dir)
+    """Test _get_bundles."""
     path = empty_dir
     dir_names = ['one', 'two', 'three']
     for name in dir_names:
@@ -80,21 +70,13 @@ def test_get_bundles(empty_dir, monkeypatch):
         assert f in dir_names
 
 
-def test_init(empty_dir, monkeypatch):
-    monkeypatch.setattr(cb, "get_repo", lambda: empty_dir)
-    bundle = 'testbundle'
-    cb.init(bundle)
-    assert (empty_dir / bundle).is_dir()
-    with pytest.raises(click.exceptions.Exit):
-        cb.init(bundle)
-
-
 def test_move(empty_dir):
-
+    """Test _move."""
     def write_test_file(filename):
         with open(filename, 'w') as file:
             file.writelines(['dummy content', 'two lines'])
 
+    # Test moving to a file
     src_file = empty_dir / "srcfile"
     dest_file = empty_dir / "destfile"
     write_test_file(src_file)
@@ -103,13 +85,46 @@ def test_move(empty_dir):
     assert not src_file.exists()
     assert dest_file.exists()
 
-def test_copy(empty_dir):
+    # Test moving into a dir
+    dest_dir = empty_dir / "destdir"
+    dest_dir.mkdir()
+    cb._move(dest_file, dest_dir)
+    assert Path(dest_dir / dest_file.name).exists()
 
+
+def test_link_back():
+    """Test _link_back"""
+    pass
+
+def test_bundle_file(test_text_file, empty_dir):
+    monkeypatch.setattr(cb, "get_bundle", lambda x: empty_dir)
+    cb._bundle_file(test_text_file, empty_dir)
+    moved_file = empty_dir / test_text_file.name
+    # backlink = cb._suffix(moved_file)
+    subprocess.call(["tree", str(test_text_file.parent)])
+    subprocess.call(["tree", str(empty_dir)])
+    assert test_text_file.is_symlink()
+    assert test_text_file.resolve() == moved_file
+
+
+# -----------------------------------------------------------
+# Test CMDs:
+
+def test_cmd_init():
+    pass
+
+
+def test_cmd_add():
+    pass
+
+
+def test_cmd_copy(empty_dir):
+    """Test copy"""
     def write_test_file(filename):
         with open(filename, 'w') as file:
             file.writelines(['dummy content', 'two lines'])
 
-    # Copy to file:
+    # Test copy to file:
     src_file = empty_dir / "srcfile"
     dest_file = empty_dir / "destfile"
     write_test_file(src_file)
@@ -117,23 +132,27 @@ def test_copy(empty_dir):
     cb._copy(src_file, dest_file)
     assert src_file.exists()
     assert dest_file.exists()
-    # Copy to dir:
+
+    # Test copy to dir:
     Path(empty_dir / "testdir").mkdir()
     dest_file = empty_dir / "testdir"
     cb._copy(src_file, dest_file)
     assert Path(dest_file / src_file).exists()
 
 
-def test_rm(empty_dir, monkeypatch):
-    monkeypatch.setattr(cb, "get_bundle", lambda x: empty_dir)
+def test_cmd_restore():
+    pass
 
+
+def test_cmd_rm(empty_bundle):
+    """Test rm"""
     def write_test_file(filename):
         with open(filename, 'w') as file:
             file.writelines(['dummy content', 'two lines'])
 
-    testfile = empty_dir / "testfile"
+    testfile = empty_bundle / "testfile"
     linkfile = cb._suffix(testfile)
-    write_test_file(empty_dir / "testfile")
+    write_test_file(empty_bundle / "testfile")
     linkfile.symlink_to(testfile)
 
     cb.rm(IGNORE_BUNDLE_ARG, testfile)
@@ -141,7 +160,7 @@ def test_rm(empty_dir, monkeypatch):
     assert not linkfile.exists()
 
 
-def test_rmdir(empty_dir, monkeypatch):
+def test_cmd_rmdir(empty_dir, monkeypatch):
     monkeypatch.setattr(cb, "get_bundle", lambda x: empty_dir)
 
     def write_test_file(filename):
@@ -157,20 +176,9 @@ def test_rmdir(empty_dir, monkeypatch):
     assert not empty_dir.exists()
 
 
-def test_link_back(test_text_file, empty_dir):
-    link_file = Path(empty_dir / "linkfile")
-    cb._link_back(link_file, test_text_file)
-    link_file = cb._suffix(link_file)
-    assert link_file.exists()
-    assert link_file.resolve() == test_text_file
+def test_cmd_destroy(test_text_file, empty_dir):
+    pass
 
-def test_bundle_file(test_text_file, empty_dir):
-    cb._bundle_file(test_text_file, empty_dir)
-    moved_file = empty_dir / test_text_file.name
-    # backlink = cb._suffix(moved_file)
-    subprocess.call(["tree", str(test_text_file.parent)])
-    subprocess.call(["tree", str(empty_dir)])
-    assert test_text_file.is_symlink()
-    assert test_text_file.resolve() == moved_file
 
-#    monkeypatch.setattr(cb, "get_bundle", lambda x: empty_dir)
+def test_cmd_path():
+    pass
